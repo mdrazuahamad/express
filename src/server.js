088ddf39a13438ef701762c48268eac3d8b5984b
Md Razu Ahamad <mdrazuahamad8@gmail.com>
2024-04-25 23:38:34 +0600

    raffel-drwa-app

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require ('express')
+
+const cors = require ('cors');
+const morgan = require ('morgan');
+
+const app = express()
+
+app.use([morgan('dev'), cors(), express.json()])
+
+
+app.get('./health', (_req,res) => {
+    res.status(200).json({message:'Success'});
+});
+
+app.use((req,res) =>{
+    const error =new Error('Resource Not Found');
+    error.status = 404;
+    next(error)
+})
+
+app.use((error,_req,res,_next) =>{
+    if (error.status){
+        return res.status(error.status).json({
+            message:error.message,
+        })
+    }
+
+    res.status(500).json({message:'Something Went Wrong'})
+})
+
+const port = process.env.Port || 4000;
+app.listen(port,()=>{
+    console.log('Server is listening on PORT',port);
+});
